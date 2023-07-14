a95adf31c3541b4a89c5e35aa1bfcc3ae55e0104
KarryCMT <1570606465@qq.com>
2023-07-14 21:55:58 +0800

    fix: 更改API地址

--- src/apis/home/index.ts
+++ src/apis/home/index.ts
@@ -3,7 +3,7 @@ import {request} from '@/utils/index.ts';
 // 获取首页列表数据
 export function getHomeList(data: object) {
   return request({
-    url: '/rabbit/system/job/find/page',
+    url: '/rabbit/business/note/find/page',
     method: 'post',
     data,
   });
