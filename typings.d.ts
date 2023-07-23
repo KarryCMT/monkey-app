f92ced86c5b49f2001c0ad7bedc4202c3af67789
KarryCMT <1570606465@qq.com>
2023-07-23 14:30:42 +0800

    chore: 使用flutter重构项目

--- typings.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.webp';
-declare module '*.json';
-declare module '*.ts';
-declare module '*.js';
-declare module '*.tsx';
